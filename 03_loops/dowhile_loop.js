d3104d85c99e767a4a2c74d6ea047d3e6e4502d5
r3yz0n <reyzon.thapa2020@gmail.com>
2025-01-01 13:54:19 +0545

    add do-while loop example with variable increment

--- 03_loops/dowhile_loop.js
+++ 03_loops/dowhile_loop.js
@@ -3,6 +3,9 @@ let id;
 let isVerfied = false;
 
 do {
+  let a = 1;
   console.log("Hello World");
+  console.log(a);
+  a++;
   x++;
-} while (x < 1); // condition lekheney thau
+} while (x <= 5); // condition lekheney thau
