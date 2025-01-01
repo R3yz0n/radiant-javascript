a99eb43ece5687efce49d377dd71c70524e7bed5
r3yz0n <reyzon.thapa2020@gmail.com>
2025-01-01 13:52:33 +0545

    create for loop

---
+++ 03_loops/for_loop.js
@@ -0,0 +1,16 @@
+// When to use for loop
+
+// 1. When you know how many times you want to run a loop
+// 2. When you want to iterate over an array or object
+
+// Syntax
+
+for (let i = 0; i < 5; i++) {
+  //   console.log(i); //0
+}
+
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++) {
+    console.log("how many times do i run?");
+  }
+}
