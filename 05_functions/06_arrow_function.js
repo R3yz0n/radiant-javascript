f5f4ef3c7ac5b0b568c88a54b9bcb1003aec51ce
r3yz0n <reyzon.thapa2020@gmail.com>
2025-01-28 12:54:28 +0545

    add examples of traditional and arrow function syntax

--- 05_functions/06_arrow_function.js
+++ 05_functions/06_arrow_function.js
@@ -1,3 +1,21 @@
-// const  =()=>{
+// Traditional function syntax
+add(1, 2);
+function add(a, b) {
+  return a + b;
+}
 
-// }
+// Arrow function syntax
+
+const sum = (a, b, c) => {
+  return a + b + c;
+};
+let result = sum(1, 2, 3);
+// console.log(result);
+result = sum(3, 3, 3);
+// console.log(result);
+
+// Find a square of a number using function with arrow syntax
+
+const findSquare = () => {
+  return;
+};
